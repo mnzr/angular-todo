84ffda210cb371b7e812ea2623fbdbc48037ed45
Ratul Minhaz <minhaz.ratul@gmail.com>
2015-10-26 12:50:23 +0600

    Added new views

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTodoApp
+ * @description
+ * # angularTodoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTodoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/users', {
+        templateUrl: 'views/users.html',
+        controller: 'UsersCtrl',
+        controllerAs: 'users'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTodoApp
+ */
+angular.module('angularTodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3','Item 4'];
+    $scope.addTodo = function () {
+      try {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+        }
+      catch(err) {
+            alert(err.message);
+        }
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todor.splice(index, 1);
+    };
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
